8ca53589e2bc6ff6b51a51bec3779f9f6b1da271
teu0508 <mateuspadilhaluz@gmail.com>
2026-04-20 23:58:54 +0100

    structure

--- RMSprop.py
+++ RMSprop.py
@@ -1,6 +1,15 @@
 
 
 class Optimizer_RMSProp:
+    
     def __init__():
         pass
     
+    def update_params():
+        pass
+    
+    def pre_update_params():
+        pass
+    
+    def post_update_params():
+        pass
