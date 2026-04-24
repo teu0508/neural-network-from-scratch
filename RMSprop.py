0df903bdfb83cad226bd6144587f09fd892d458a
teu0508 <mateuspadilhaluz@gmail.com>
2026-04-24 23:44:43 +0100

    pre and post updated

--- RMSprop.py
+++ RMSprop.py
@@ -2,15 +2,20 @@
 
 class Optimizer_RMSProp:
     
-    def __init__(self, learning_rate=0.001, decay, epsilon, rho):
+    def __init__(self, learning_rate=0.001, decay=0., epsilon=1e-7, rho=0.9):
         self.learning_rate = learning_rate
-        
+        self.current_learning_rate = learning_rate
+        self.decay = decay
+        self.iterations = 0
+        self.epsilon = epsilon
+        self.rho = rho
     
-    def update_params():
+    def update_params(self):
         pass
     
-    def pre_update_params():
-        pass
+    def pre_update_params(self):
+        if self.decay:
+            self.current_learning_rate = self.learning_rate * (1. / (1. + self.decay * self.iterations))
     
-    def post_update_params():
-        pass
+    def post_update_params(self):
+        self.iterations += 1
