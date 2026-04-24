89ee0a7a42e340f8252fa804b68c312ba9cb40e9
teu0508 <mateuspadilhaluz@gmail.com>
2026-04-24 23:41:46 +0100

    parameters

--- RMSprop.py
+++ RMSprop.py
@@ -2,8 +2,9 @@
 
 class Optimizer_RMSProp:
     
-    def __init__():
-        pass
+    def __init__(self, learning_rate=0.001, decay, epsilon, rho):
+        self.learning_rate = learning_rate
+        
     
     def update_params():
         pass
