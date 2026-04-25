4f005aaf1e2bac0189d5a1e288f8cc03e3df9ee6
teu0508 <mateuspadilhaluz@gmail.com>
2026-04-25 21:25:49 +0100

    adaptive momentum optimizer

---
+++ Adaptive_Momentum.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+class Optimizer_Adam:
+    
+    def __init__(self , learning_rate = 0.001 , decay = 0. , epsilon = 1e-7 ,beta_1 = 0.9 , beta_2 = 0.999):
+        self.learning_rate = learning_rate
+        self.current_learning_rate = learning_rate
+        self.decay = decay
+        self.iterations = 0
+        self.epsilon = epsilon
+        self.beta1 = beta_1
+        self.beta2 = beta_2
+        
+    def pre_update_params(self):
+        if self.decay:
+            self.current_learning_rate = self.learning_rate * (1. / (1. + self.decay * self.iterations))
+
+    def update_params(self, layer):
+        pass
+    
+    def post_update_params(self):
+        self.iterations += 1
