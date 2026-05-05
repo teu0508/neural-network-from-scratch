9855fe2ad74f6bed9bf08e77303478e521f83736
teu0508 <mateuspadilhaluz@gmail.com>
2026-05-05 10:26:04 -0400

    klkl

--- Layer_Dense.py
+++ Layer_Dense.py
@@ -6,10 +6,14 @@ class Layer_Dense:
     
     # Initializing weights and biases 
     # For now these are random just for the sake of constructing a network of neurons
-    def __init__(self, n_inputs, n_neurons):
-        pass 
+    def __init__(self, n_inputs, n_neurons, weight_regularizer_l1=0, weight_regularizer_l2=0, bias_regularizer_l1=0, bias_regularizer_l2=0):
         self.weights = np.random.randn(n_inputs, n_neurons) * 0.01 
         self.biases = np.zeros((1, n_neurons))
+        #defning regularization strength
+        self.weight_regulaizer_l1 = weight_regularizer_l1
+        self.weight_regulaizer_l2 = weight_regularizer_l2
+        self.bias_regularizer_l1 = bias_regularizer_l1
+        self.bias_regularizer_l2 = bias_regularizer_l2
     
     # Forward pass
     # Calculating the outputs based on the inputs, weights and biases
